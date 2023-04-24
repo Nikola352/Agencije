066d584865edc5e304e3d31e0c4d75953aaed2c5
Nikola Jolovic <nikolajolovic02@gmail.com>
2023-04-24 12:38:22 +0200

    fix: footer positioning

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,24 @@ import { UserProvider } from './data/UserContext'
 import NotFound from './pages/NotFound'
 import AgencijaPage from './pages/AgencijaPage'
 import Footer from './layouts/Footer'
+import Admin from './pages/Admin'
 
 function App() {
   return (
     <Router>
         <UserProvider>
-            <div className="App">
+            <div id="App" className='relative min-h-[100vh]'>
                 <Navbar />
 
                 <div className='h-10 sm:h-12 mb-4'></div>
 
-                <main className="content p-4 -z-10 text-secondary-800">
+                <main className="content p-4 pb-16 sm:pb-20 -z-10 text-secondary-800">
                     <Routes>
                         <Route path='/' element={<Home/>} />
                         <Route path='/agencija/:id' element={<AgencijaPage/>} />
                         <Route path='*' element={<NotFound/>} />
                     </Routes>
                 </main>
-
-                <div className='h-10 sm:h-12 mt-4'></div>
                 
                 <Footer />
             </div>
--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <footer id="footer" className="fixed bottom-0 w-full h-10 sm:h-12 border-t-2 bg-secondary-400 border-secondary-600">
+        <footer id="footer" className="absolute bottom-0 w-full h-10 sm:h-12 mt-auto border-t-2 bg-secondary-400 border-secondary-600">
 
             <p className="text-center mt-2 text-white">
                 <span className="font-bold">© Agencije</span> | Designed by Nikola Jolović
